cf8d8e505cd29c220948bad0961081e0cb956e68
Rafael-129 <rafael.garcia.c@tecsup.edu.pe>
2025-04-09 14:25:58 -0500

    Cambios Views

--- src/animeapp/views.py
+++ src/animeapp/views.py
@@ -1,3 +1,14 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
+def buscar_anime(request):
+    query =request.Get.get('q')
+    resulador = []
+    if query:
+         url = f'https://api.jikan.moe/v4/anime?q={query}&limit=10'
+         response = request.get(url)
+         
+         if response.status_code == 200:
+              resultados = response.json().get('data', [])
+    return render(request, "animeap/home.html", {'anmesapp': resultados})
